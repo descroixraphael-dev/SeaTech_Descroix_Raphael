1a440f96c76b99c605c9ad09a8a7eaa0429c0916
descroixraphael-dev <descroixraphael@gmail.com>
2025-12-04 14:59:45 +0100

    Add HandleStartButton function to manage start state

--- embedded/Robot_Ledunois_Descroix.X/Robot.c
+++ embedded/Robot_Ledunois_Descroix.X/Robot.c
@@ -1,3 +1,8 @@
 #include "Robot.h"
 #include "main.h"
 volatile ROBOT_STATE_BITS robotState;
+void HandleStartButton(void){
+  if (robotState.isStarted == 0 && BOUTON_START == 0){
+      robotState.isStarted = 1;
+  }
+}
